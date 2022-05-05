45a0d56cc6a35029124822f7d66108802df45cc1
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-05 22:20:24 +0200

    Repository

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,19 @@
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+  public interface ICommandRepo
+  {
+    bool SaveChanges();
+
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform platform);
+    bool PlatformExist(int platformId);
+
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+  }
+}
