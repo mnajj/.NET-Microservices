ded0065dce2b49590aee4a463106e6c25d6d5b76
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-11 18:26:11 +0200

    Add Event Listner

--- CommandsService/EventProcessing/EventProcessor.cs
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -25,7 +25,7 @@ namespace CommandsService.EventProcessing
       switch (eventType)
       {
         case EventType.PlatformPublished:
-          // TODO
+          AddPlatform(message);
           break;
         default:
           // TODO
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemeoryDb"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+builder.Services.AddHostedService<MessageBusSubscriber>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
