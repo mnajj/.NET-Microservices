4026a6c0d8278ff7e79b2059f8244cef9365517e
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-13 17:40:50 +0200

    Adding a gRPC Client to Commands Service

---
+++ CommandsService/SyncDataServices/Grpc/PatformDataClient.cs
@@ -0,0 +1,36 @@
+using AutoMapper;
+using CommandsService.Models;
+using Grpc.Net.Client;
+using PlatformService;
+
+namespace CommandsService.SyncDataServices.Grpc
+{
+  public class PatformDataClient : IPlatformDataClient
+  {
+    private readonly IConfiguration _config;
+    private readonly IMapper _mapper;
+
+    public PatformDataClient(IConfiguration config, IMapper mapper)
+    {
+      _config = config;
+      _mapper = mapper;
+    }
+    public IEnumerable<Platform> ReturnAllPlatforms()
+    {
+      var channel = GrpcChannel.ForAddress(_config["GrpcPlatform"]);
+      var client = new GrpcPlatform.GrpcPlatformClient(channel);
+      var req = new GetAllRequest();
+
+      try
+      {
+        var repy = client.GetAllPlatforms(req);
+        return _mapper.Map<IEnumerable<Platform>>(repy.Platform);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"==> Couldn't call gRPC Server: {ex.Message}");
+        return null;
+      }
+    }
+  }
+}
